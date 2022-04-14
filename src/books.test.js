73dd3aea8a4706365eddac334ef72d618781938c
Abdel Raoof <olakara@gmail.com>
2022-04-14 21:50:04 +0400

    Initial commit

---
+++ src/books.test.js
@@ -0,0 +1,6 @@
+import BooksPresenter from "./Books/BooksPresenter";
+import httpGateway from "./Shared/HttpGateway";
+
+it("should load private books", async () => {});
+
+it("should load public books", async () => {});
